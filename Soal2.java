ae62c89c82fff33e367331c4ed3162b1ef099a1a
MuhammadZulfikarIkhsan <71245150+MuhammadZulfikarIkhsan@users.noreply.github.com>
2020-09-21 17:06:37 +0700

    Delete Soal2.java

--- Soal2.java
+++
@@ -1,8 +0,0 @@
-public class Soal2{
-   public static void main(String[]args){ 
-      int umur = 17;
-      String name = "Muhammad Zulfikar Ikhsan";
-      System.out.println("Halo," + name);
-      System.out.println("Ternyata usiamu baru " + umur + " tahun ya");
-   }
-}
